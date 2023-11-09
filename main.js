9abc358d206b77758376f30c11756d5778d56af1
Ksenia Konovtseva <ksenia1229@mail.ru>
2023-11-10 00:39:26 +0300

    lesson16

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
 'use strict';
+
+class First {
+  constructor (str) {
+    this.str = str
+  }
+  hello() {
+    console.log(`${this.str}`);
+  }
+}
+
+class Second extends First {
+  constructor (str, secondStr) {
+    super (str)
+    this.secondStr = secondStr
+  }
+  helloSecond() {
+    super.hello();
+    console.log(this.secondStr);
+  }
+}
+
+const second = new Second('Я метод родителя', 'Я приемный');
+second.helloSecond();
